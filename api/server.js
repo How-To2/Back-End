470530684f8dc2eba4ee81f05a73e9195aea6ce3
Edwin Chajon <junchajo@gmail.com>
2020-03-02 10:30:10 -0800

    set

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require("cors");
 
 const server = express();
 
 server.use(helmet());
 
+server.use(cors());
 
 server.get('/', (req, res) => {
     res.send(`api is up`)
